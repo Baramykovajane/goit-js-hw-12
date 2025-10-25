1a950d41204bbb7b84c7c31ef1347cd4d22f8e54
unknown <baramikovajane@gmail.com>
2025-10-25 09:26:02 +0300

    change js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,25 +3,21 @@ const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "52792458-b846233229dd28b7897b8b61b";
 const PER_PAGE = 15;
 export async function getImagesByQuery(query, page = 1) {
-try {
-const url = new URL(BASE_URL);
-url.searchParams.set('key', API_KEY);
-url.searchParams.set('q', query);
-url.searchParams.set('image_type', 'photo');
-url.searchParams.set('orientation', 'horizontal');
-url.searchParams.set('safesearch', 'true');
-url.searchParams.set('per_page', PER_PAGE.toString());
-url.searchParams.set('page', String(page));
+  try {
+    const response = await axios.get(BASE_URL, {
+       params: {
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+      },
+    });
 
 
-const response = await fetch(url.toString());
-if (!response.ok) {
-throw new Error(`HTTP error! status: ${response.status}`);
-}
-const data = await response.json();
-return data; 
-} catch (error) {
-console.error('Error fetching images:', error);
-throw error;
-}
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    throw error;
+  }
 }
